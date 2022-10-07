99184a6690b376fd332d088c3919d2bdf2374f30
SerhiiBakun <104979057+SerhiiBakun@users.noreply.github.com>
2022-10-07 16:56:54 +0000

    Deploying to gh-pages from @ SerhiiBakun/goit-js-hw-09@6c5d4a59c8f8b3ada259b370a1ea77e36073fb11 🚀

--- 01-color-switcher.9e76ee4e.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},n=null;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.9e76ee4e.js.map
